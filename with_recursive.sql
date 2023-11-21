d7b5d3435b4a5df35ca77e28ce587bbebff64edc
Rahul Singh Chauhan <rahulsinghchauhan14@gmail.com>
2023-11-21 16:49:35 +0530

    Update with_recursive.sql

--- with_recursive.sql
+++ with_recursive.sql
@@ -36,3 +36,22 @@ output:
 select p.*, generate_series
 FROM job_positions p 
 cross join generate_series(1,p.totalpost);
+
+
+-- Another query
+with recursive cte as
+		(
+      select p.id, p.title, p.groups, p.levels, p.payscale, '' as employee_name, p.totalpost 
+		    from job_positions p 
+		    union all
+		      select id, title, groups, levels, payscale, '' as employee_name, (totalpost - 1) as totalpost
+		        from cte 
+		            where totalpost > 1
+    )
+	  select title, groups, levels, payscale, coalesce(e.name,'Vacant') as employee_name
+	  from cte
+	  left join (
+                select *, row_number() over(partition by position_id order by id) as rn 
+			          from job_employees
+              ) e on e.rn=cte.totalpost and e.position_id = cte.id
+	  order by groups,totalpost;
